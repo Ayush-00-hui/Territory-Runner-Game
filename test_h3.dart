80a9a6bd1c5fc39390e19942dc29178c08b16356
Ayush Arya <ayusharya.25a@gmail.com>
2026-09-16 18:53:58 +0530

    chore: remove temporary test files

--- test_h3.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:mirrors';
-import 'package:h3_ffi/h3_ffi.dart';
-
-void main() {
-  final h3 = const H3FfiFactory().build(); 
-  // Wait, I can't guess the class name.
-  print('done');
-}
